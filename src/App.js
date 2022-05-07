ebcca3d9e3c863a0d71a9e0e5aafac304b646007
JoDamaso <darryldamaso@gmail.com>
2022-05-07 17:28:35 -0500

    initial components added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Header from './Header';
+import Nav from './Nav';
+import Project from './Project';
+import Footer from './Footer';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header>
+        <Nav></Nav>
+      </Header>
+      <main>
+        <Project>
+
+        </Project>
+      </main>
+      <Footer></Footer>
     </div>
+
   );
 }
 
