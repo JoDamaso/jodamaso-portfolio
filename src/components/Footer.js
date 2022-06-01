bc4b5f12f38ff386de6825c2333ca4ac8523f6c2
JoDamaso <darryldamaso@gmail.com>
2022-05-31 22:35:01 -0500

    trying to deploy again

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Footer() {
-    return (
-        <footer className="d-flex justify-content-center">Thanks for visting!</footer>
-    )
-}
-
-export default Footer
